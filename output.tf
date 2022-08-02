12599eada584e2fec7b00cc6e08763d1c78745cc
alexandre <alexandre.em@monkvision.ai>
2022-08-02 23:39:06 +0200

    ]Add heroku deploy config

---
+++ output.tf
@@ -0,0 +1,5 @@
+output "app_url" {
+  value       = heroku_app.kanjiupapi.web_url
+  description = "Application URL"
+}
+
---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 4.6.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
+
