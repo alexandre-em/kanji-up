2b1ba8861bea067de1426db6d042eee9e9f6eb67
Alexandre <em_alexandre@yahoo.co.jp>
2022-12-12 17:18:13 +0100

    Fix port env name

--- back/auth/src/main.ts
+++ back/auth/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.set('view engine', 'hbs');
 
-  await app.listen(parseInt(process.env.AUTH_API_PORT || '3000'));
+  await app.listen(parseInt(process.env.PORT || '3000'));
 }
 bootstrap();
