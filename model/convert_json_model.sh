9cf1a52de278b1e4244c3f84e2d0c99e4a8722c4
Alexandre <em_alexandre@yahoo.co.jp>
2022-12-20 19:10:02 +0100

    Fix app crash on model load (android)
    Signed-off-by: Alexandre <em_alexandre@yahoo.co.jp>
    
    Update get model url route & model conversion
    
    Signed-off-by: Alexandre <em_alexandre@yahoo.co.jp>
    
    Fix Android crash when loading model

--- model/convert_json_model.sh
+++ model/convert_json_model.sh
@@ -1,6 +1,11 @@
 tensorflowjs_converter \
     --input_format=tf_saved_model \
-    --output_node_names='KanjiRecogniser/Predictions/Reshape_1' \
+    --output_node_names='KanjiRecognition/Predictions/Reshape_1' \
     --saved_model_tags=serve \
     ./kanji_saved_model \
     ./kanji_web_model
+
+#tensorflowjs_converter \
+#    --input_format=keras \
+#    ./kanji_model_ok.h5 \
+#    ./kanji_web_model
