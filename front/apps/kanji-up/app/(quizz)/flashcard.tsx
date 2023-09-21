420b5b2de9738addbfc154a6d0ceba46ebc8bf7d
Alexandre <em_alexandre@yahoo.co.jp>
2023-09-21 15:36:32 +0200

    Add flip animation on flashcard

--- front/apps/kanji-up/app/(quizz)/flashcard.tsx
+++ front/apps/kanji-up/app/(quizz)/flashcard.tsx
@@ -1,17 +1,19 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Text, useWindowDimensions, View } from 'react-native';
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { Button, Divider, ProgressBar, Surface, TouchableRipple } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import { SVGUriPlatform } from 'kanji-app-svg-ui';
+import { router } from 'expo-router';
 
 import styles from './style';
 import { colors } from 'constants/Colors';
 import { RootState } from 'store';
 import { useQuizzContext } from './_layout';
-import { router } from 'expo-router';
 
 export default function Practice() {
   const { width } = useWindowDimensions();
+  const spin = useSharedValue<number>(0);
   const [counter, setCounter] = useState<number>(0);
   const [reverse, setReverse] = useState<boolean>(false);
   const settingsState = useSelector((state: RootState) => state.settings);
@@ -30,33 +32,32 @@ export default function Practice() {
     return [];
   }, [QuizzContext.kanjis, settingsState.evaluationCardNumber]);
 
-  const cardContent = useMemo(() => {
+  const cardBackContent = useMemo(() => {
     if (!kanjiQueue || (kanjiQueue && !kanjiQueue[counter])) return null;
-    if (reverse) {
-      return (
-        <View style={{ padding: 30, justifyContent: 'center', height: '100%' }}>
-          <Text style={{ color: colors.primary, fontSize: 30, fontWeight: '900', alignSelf: 'center' }}>
-            {kanjiQueue[counter].kanji!.character}
-          </Text>
-          <Text style={styles.text}>on: {kanjiQueue[counter].kanji!.onyomi}</Text>
-          <Text style={styles.text}>kun: {kanjiQueue[counter].kanji!.kunyomi}</Text>
-          <Text style={styles.text}>meaning: {kanjiQueue[counter].kanji!.meaning}</Text>
-          <Divider style={{ marginVertical: 15, width: '80%', alignSelf: 'center' }} />
-          <Text style={styles.text}>Examples</Text>
-          {kanjiQueue[counter].examples &&
-            kanjiQueue[counter].examples
-              ?.map((k) => (
-                <View key={k.japanese}>
-                  <Text style={{ fontWeight: '100', color: colors.text }}>{k.japanese}</Text>
-                  <Text style={{ fontWeight: '100', color: colors.text }}>{k.meaning}</Text>
-                </View>
-              ))
-              .filter((_, index) => index < 3)}
-        </View>
-      );
-    }
+    return (
+      <View style={{ padding: 30, justifyContent: 'center', height: '100%' }}>
+        <Text style={{ color: colors.primary, fontSize: 30, fontWeight: '900', alignSelf: 'center' }}>
+          {kanjiQueue[counter].kanji!.character}
+        </Text>
+        <Text style={styles.text}>on: {kanjiQueue[counter].kanji!.onyomi}</Text>
+        <Text style={styles.text}>kun: {kanjiQueue[counter].kanji!.kunyomi}</Text>
+        <Text style={styles.text}>meaning: {kanjiQueue[counter].kanji!.meaning}</Text>
+        <Divider style={{ marginVertical: 15, width: '80%', alignSelf: 'center' }} />
+        {kanjiQueue[counter].examples && <Text style={styles.text}>Examples</Text>}
+        {kanjiQueue[counter].examples &&
+          kanjiQueue[counter].examples
+            ?.map((k) => (
+              <View key={k.japanese}>
+                <Text style={{ fontWeight: '100', color: colors.text }}>{k.japanese}</Text>
+                <Text style={{ fontWeight: '100', color: colors.text }}>{k.meaning}</Text>
+              </View>
+            ))
+            .filter((_, index) => index < 3)}
+      </View>
+    );
+  }, [counter, kanjiQueue]);
 
-    // TODO: Check if svg is available and if not display text
+  const cardFrontContent = useMemo(() => {
     return (
       <SVGUriPlatform
         width={imgSize}
@@ -68,11 +69,34 @@ export default function Practice() {
 
   const handleReverse = useCallback(() => {
     setReverse((p) => !p);
+    spin.value = spin.value ? 0 : 1;
   }, []);
 
   const handleNext = useCallback(() => {
     setCounter((prev) => prev + 1);
   }, []);
+
+  const animatedFrontStyle = useAnimatedStyle(() => {
+    const spinVal = interpolate(spin.value, [0, 1], reverse ? [0, 180] : [180, 360]);
+    return {
+      transform: [
+        {
+          rotateY: withTiming(`${spinVal}deg`, { duration: 500 }),
+        },
+      ],
+    };
+  }, [reverse]);
+
+  const animatedBackStyle = useAnimatedStyle(() => {
+    const spinVal = interpolate(spin.value, [0, 1], reverse ? [180, 360] : [0, 180]);
+    return {
+      transform: [
+        {
+          rotateY: withTiming(`${spinVal}deg`, { duration: 500 }),
+        },
+      ],
+    };
+  }, [reverse]);
 
   useEffect(() => {
     if (counter >= settingsState.flashcardNumber) {
@@ -96,7 +120,15 @@ export default function Practice() {
         />
       </Surface>
       <TouchableRipple style={{ width: imgSize, height: imgSize, alignSelf: 'center' }} onPress={handleReverse}>
-        <Surface style={{ width: imgSize, height: imgSize }}>{cardContent}</Surface>
+        {reverse ? (
+          <Animated.View style={[animatedFrontStyle]}>
+            <Surface style={{ width: imgSize, height: imgSize }}>{cardFrontContent}</Surface>
+          </Animated.View>
+        ) : (
+          <Animated.View style={[animatedBackStyle]}>
+            <Surface style={{ width: imgSize, height: imgSize }}>{cardBackContent}</Surface>
+          </Animated.View>
+        )}
       </TouchableRipple>
 
       <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
