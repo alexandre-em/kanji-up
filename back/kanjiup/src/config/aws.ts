449f9a8f9381ef978f1da85f67c5c87b5680743d
Alexandre <em_alexandre@yahoo.co.jp>
2024-07-23 22:22:25 +0200

    Upgrade aws-sdk version 2 to 3

--- back/kanjiup/src/config/aws.ts
+++ back/kanjiup/src/config/aws.ts
@@ -1,75 +1,47 @@
-import AWS from 'aws-sdk';
+import { ServiceException } from '@smithy/smithy-client';
+
+import { DeleteObjectCommand, DeleteObjectCommandInput, DeleteObjectCommandOutput, PutObjectCommand, PutObjectCommandInput, S3Client } from '@aws-sdk/client-s3';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const createS3 = (): AWS.S3 =>
-  new AWS.S3({
-    endpoint: `https://${process.env.CF_ACC_ID}.r2.cloudflarestorage.com`,
-    accessKeyId: process.env.AWS_ACCESS_ID,
-    secretAccessKey: process.env.AWS_ACCESS_SECRET,
-    signatureVersion: 'v4',
-  });
+export const storageUrl = `${process.env.STORAGE_BASE_URL}`;
+const endpoint = `https://${process.env.CF_ACC_ID}.r2.cloudflarestorage.com`;
+export const Bucket = process.env.AWS_BUCKET_NAME || '';
 
-export const createBucket = (name: string): Promise<AWS.S3.CreateBucketOutput | AWS.AWSError> => {
-  const s3 = createS3();
+const client = new S3Client({
+  endpoint,
+  region: 'weur',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_ID || '',
+    secretAccessKey: process.env.AWS_ACCESS_SECRET || '',
+  },
+});
 
-  const params = {
-    Bucket: name,
-    CreateBucketConfiguration: {
-      LocationConstraint: 'eu-west-3',
-    },
-  };
-  return new Promise((resolve, reject) => {
-    s3.createBucket(params, function(err, data) {
-      if (err) {
-        console.log(err, err.stack);
-        reject(err);
-      } else {
-        console.log('Bucket Created Successfully', data.Location);
-        resolve(data);
-      }
-    });
-  });
-};
-
-export const uploadFile = (fileName: string, content: Buffer, customParams: Partial<AWS.S3.PutObjectRequest> = {}): Promise<AWS.S3.ManagedUpload.SendData | Error> => {
-  const s3 = createS3();
-
-  const params: AWS.S3.PutObjectRequest = {
-    Bucket: process.env.AWS_BUCKET_NAME || '',
+export const uploadFile = async (fileName: string, content: Buffer, customParams: Partial<PutObjectCommandInput> = {}): Promise<any | Error> => {
+  const params: PutObjectCommandInput = {
+    Bucket,
     Key: fileName,
     Body: content,
     ...customParams,
   };
 
-  return new Promise((resolve, reject) => {
-    s3.upload(params, function(err, data) {
-      if (err) {
-        reject(err);
-      }
-      // console.log(`File uploaded successfully. ${data.Location}`);
-      resolve(data);
-    });
-  });
+  const command = new PutObjectCommand(params);
+
+  await client.send(command);
+
+  return `${storageUrl}/${Bucket}/${fileName}`;
 };
 
-export const deleteFile = (fileName: string, customParams: Partial<AWS.S3.DeleteObjectRequest> = {}): Promise<AWS.S3.DeleteObjectOutput | AWS.AWSError> => {
-  const s3 = createS3();
-
-  const params: AWS.S3.DeleteObjectRequest = {
+export const deleteFile = async (fileName: string, customParams: Partial<DeleteObjectCommandInput> = {}): Promise<DeleteObjectCommandOutput | ServiceException | null> => {
+  const params: DeleteObjectCommandInput = {
     Bucket: process.env.AWS_BUCKET_NAME || '',
     Key: fileName,
     ...customParams,
   };
 
-  return new Promise((resolve, reject) => {
-    s3.deleteObject(params, function(err, data) {
-      if (err) {
-        reject(err);
-      }
-      console.log(`File deleted successfully. ${fileName}`);
-      resolve(data);
-    });
-  });
+  const command = new DeleteObjectCommand(params);
+
+  return await client.send(command);
 };
