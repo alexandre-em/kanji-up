495b5006c0f6667cb5e3179254917d175045f30c
Alexandre <em_alexandre@yahoo.co.jp>
2024-10-28 21:09:07 +0100

    Config tailwind + shadcn on kanji app

--- front/web/kanji-app/src/pages/KanjiUpAppPage.tsx
+++ front/web/kanji-app/src/pages/KanjiUpAppPage.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { PageLayout, TypographyH2, TypographyP } from 'gatewayApp/shared';
 
 export default function KanjiHome() {
-  return <div>Welcome to Kanji app</div>;
+  return (
+    <PageLayout>
+      <div>
+        <div>
+          <TypographyP>Hello Alexandreさん</TypographyP>
+          <TypographyH2>Let&apos;s practice !</TypographyH2>
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
