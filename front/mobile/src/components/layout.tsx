112a2eed50cdfc5a8be94bc7ab617cc97bc71a3a
Alexandre Em <a.em@qivia.fr>
2025-09-07 16:33:35 +0200

    feat: update layout component to have title & subtitle

--- front/mobile/src/components/layout.tsx
+++ front/mobile/src/components/layout.tsx
@@ -1,9 +1,31 @@
 import { PropsWithChildren } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ScrollView, StyleSheet } from 'react-native';
-import { Colors } from 'react-native-ui-lib';
+import { Colors, Text, View } from 'react-native-ui-lib';
 
-export default function Layout({ children }: PropsWithChildren) {
-  return <ScrollView style={styles.container}>{children}</ScrollView>;
+import Spacing from './spacing';
+
+type LayoutProps = {
+  screen?: string;
+};
+
+export default function Layout({ screen, children }: LayoutProps & PropsWithChildren) {
+  const { t } = useTranslation();
+
+  const title = t(`${screen}.title`);
+  const subtitle = t(`${screen}.subtitle`);
+
+  return (
+    <ScrollView style={styles.container}>
+      <View height="100%">
+        {title !== `${screen}.title` && <Text h1>{title}</Text>}
+        {title !== `${screen}.title` && subtitle !== `${screen}.subtitle` && <Spacing y={5} />}
+        {subtitle !== `${screen}.subtitle` && <Text text80L>{t(`${screen}.subtitle`)}</Text>}
+        {title !== `${screen}.title` && subtitle !== `${screen}.subtitle` && <Spacing y={10} />}
+        {children}
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
